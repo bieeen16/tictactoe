d3690a59b1a4d187fb414b2676702fc8b0c9cf78
Bien Acamad <bienacamad@Biens-MacBook-Air.local>
2024-02-11 14:33:46 +0800

    updated

--- server/src/model/GameData.js
+++ server/src/model/GameData.js
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
 
-const gameDataSchema = new mongoose.Schema({
-  matchData: [
-    {
-      matchNumber: [{ type: String, required: true }],
-      matchHistory: [{ type: String, required: true }],
-    },
-  ],
-  player1Name: { type: String, required: true },
-  player2Name: { type: String, required: true },
-  p1Wins: { type: Number, required: true },
-  p2Wins: { type: Number, required: true },
-  p1Losses: { type: Number, required: true },
-  p2Losses: { type: Number, required: true },
-  p1Draws: { type: Number, required: true },
-  p2Draws: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const gameDataSchema = new mongoose.Schema(
+  {
+    matchData: [
+      {
+        matchNumber: [{ type: String, required: true }],
+        matchHistory: [{ type: String, required: true }],
+      },
+    ],
+    player1Name: { type: String, required: true },
+    player2Name: { type: String, required: true },
+    p1Wins: { type: Number, required: true },
+    p2Wins: { type: Number, required: true },
+    p1Losses: { type: Number, required: true },
+    p2Losses: { type: Number, required: true },
+    p1Draws: { type: Number, required: true },
+    p2Draws: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const GameData = mongoose.model("GameData", gameDataSchema);
 
